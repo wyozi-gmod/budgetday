45cdc3e17c67e8881a75863166457d64d737e726
wyozi <joonasmertanen@gmail.com>
2014-11-21 20:03:00 +0200

    Remove not being able to drag body when standing

--- gamemode/bdmodules/sv_bodies.lua
+++ gamemode/bdmodules/sv_bodies.lua
@@ -14,18 +14,26 @@ hook.Add("Think", "BDHandleBodyDrag", function()
 			if IsValid(phys) then
 				local vec = target - phys:GetPos()
 				local len = vec:Length()
-				if len > 40 or not ply:KeyDown(IN_DUCK) then
+				if len > 50 then
 					ply.BD_DragData = nil
 					dd.body:SetNWBool("BeingDragged", false)
 					return
 				end
 				vec:Normalize()
-				local tvec = vec * len * 15
+				local tvec = vec * len * 20
 				local avec = tvec - phys:GetVelocity()
 				avec = avec:GetNormal() * math.min(45, avec:Length())
 				avec = avec / phys:GetMass() * 16
 				phys:AddVelocity(avec)
 			end
 		end
+	end
+end)
+
+hook.Add("FinishMove", "BD.DropBody", function(ply, mv)
+	local btns = mv:GetButtons()
+	if (bit.band(btns, IN_ATTACK) == IN_ATTACK or bit.band(btns, IN_ATTACK2) == IN_ATTACK2) and ply.BD_DragData and IsValid(ply.BD_DragData.body) then
+		ply.BD_DragData.body:SetNWBool("BeingDragged", false)
+		ply.BD_DragData = nil
 	end
 end)
