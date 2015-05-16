c6cd41e404144688a827a0d3a56a5a90e2f503f0
wyozi <joonasmertanen@gmail.com>
2015-05-17 00:37:07 +0300

    Dampen jump speed instead of preventing jumping completely on high velocities

--- gamemode/bdmodules/sh_player_misc.lua
+++ gamemode/bdmodules/sh_player_misc.lua
@@ -2,7 +2,7 @@
 -- Normal GMod movement is too flexible
 hook.Add("SetupMove", "BD_NerfMovement", function( ply, mv, cmd )
 	if mv:KeyDown(IN_JUMP) and (mv:GetVelocity():Length() > 175 or mv:KeyDown(IN_DUCK)) and ply:GetMoveType() == MOVETYPE_WALK then
-		mv:SetButtons(bit.band(mv:GetButtons(), bit.bnot(IN_JUMP)))
+		mv:SetVelocity(mv:GetVelocity():GetNormalized() * 175)
 	end
 end)
 
